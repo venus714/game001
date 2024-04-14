de4e9589cc501ccad7274e2c03d9795babbc4f82
v3n_us 714 <venus.kabera@student.moringaschool.com>
2024-04-14 13:18:26 +0300

    added js to make character jump

---
+++ script.js
@@ -0,0 +1,10 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
